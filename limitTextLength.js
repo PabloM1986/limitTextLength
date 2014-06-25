314e7ca8a663978fa1e952b8331e5fbc41906e98
PabloM1986 <pfmescher@gmail.com>
2014-06-25 10:58:31 -0300

    Create limitTextLength.js

---
+++ limitTextLength.js
@@ -0,0 +1,37 @@
+function limitTextLength(config) {
+    if (!config.element) {
+        throw new Error("No element provided");
+    }
+    element = jQuery(config.element);
+    var originalText = element.text(),
+        trimmedText = originalText.substring(0, config.maxLength),
+        trimmedText = trimmedText.substring(0, Math.min(trimmedText.length, trimmedText.lastIndexOf(" "))),
+        leftOut = originalText.substring(trimmedText.length),
+        hoverCTA = jQuery("<span>...</span>");
+    if (config.class) {
+        hoverCTA.addClass(config.class);
+    }
+    if (config.css) {
+        hoverCTA.css(config.css);
+    }
+    if (typeof config.enter !== "function") {
+        config.enter = function () {
+            jQuery(this).text(leftOut);
+        }
+    }
+    if (typeof config.leave !== "function") {
+        config.leave = function () {
+            jQuery(this).text("...");
+        }
+    }
+
+    hoverCTA.on("mouseenter", function () {
+        config.enter.apply(this, [leftOut, originalText]);
+    });
+
+    hoverCTA.on("mouseleave", function () {
+        config.leave.apply(this, [leftOut, originalText]);
+    });
+
+    element.text(trimmedText).append(hoverCTA);
+}
